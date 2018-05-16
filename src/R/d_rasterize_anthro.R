137199f078432f9f598f82ea65215a9395c59d78
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-15 22:45:29 -0600

    clean based on other update code syntax

--- src/R/d_rasterize_anthro.R
+++ src/R/d_rasterize_anthro.R
@@ -1,110 +1,98 @@
 
-if (!exists("ras_mask")){
-  if (!file.exists(file.path(ancillary_dir, "ras_mask/ras_mask.tif"))){
+if (!exists("ras_mask")) {
+  if (!file.exists(file.path(ancillary_dir, "ras_mask/ras_mask.tif"))) {
     if (!exists("fishnet_4k")) {
       fishnet_4k <- st_read(file.path(fishnet_path, "fishnet_4k.gpkg"))
     }
-    ras_mask <- raster(as(fishnet_4k, "Spatial"), res = 4000)
-}}
-# Calculate distance to power lines
-if (!file.exists(file.path(anthro_dir, "dis_transmission_lines.tif"))) {
-    tl <- st_read(file.path(processed, "power_lines.gpkg"))
-    
-    sfInit(parallel = TRUE, cpus = ncor)
-    sfExport(list = c("ncor", "usa_shp", "tl", "ras_mask"))
-    tl_rst <- sfLapply(1:ncor, shp_rst, y = tl, lvl = "bool_tl", j = ras_mask)
-    sfStop()
-    dis_transmission_lines <- combine_rst(tl_rst) 
-    writeRaster(dis_transmission_lines, filename = file.path(anthro_dir, paste0("dis_transmission_lines", ".tif")),
-                format = "GTiff", overwrite=TRUE)
+    ras_mask <- fishnet_4k %>%
+      st_transform(p4string_ed)
+    ras_mask <- raster(as(ras_mask, "Spatial"), res = 4000)
+  }
 }
 
+# Calculate distance to power lines
+if (!file.exists(file.path(transportation_dist_dir, "dis_transmission_lines.tif"))) {
+    tl_ed <- tl %>%
+      st_transform(p4string_ed)
+
+    sfInit(parallel = TRUE, cpus = ncor)
+    sfExport(list = c("ncor", "usa_ed", "tl_ed", "ras_mask"))
+    tl_rst <- sfLapply(1:ncor, shp_rst, y = tl_ed, lvl = "bool_tl", j = ras_mask)
+    sfStop()
+    dis_transmission_lines <- combine_rst(tl_rst)
+    writeRaster(dis_transmission_lines, filename = file.path(transportation_dist_dir, paste0("dis_transmission_lines", ".tif")),
+                format = "GTiff", overwrite=TRUE)
+} else {
+  dis_transmission_lines <- raster(filename = file.path(transportation_dist_dir, paste0("dis_transmission_lines", ".tif")))
+}
+
+
 # Calculate distance to railroads
-if (file.exists(file.path(anthro_dir, "dis_railroads.tif"))) {
-  
-  dis_railroads <- raster(file.path(anthro_dir, "dis_railroads.tif")) 
-  
-  } else {  sfInit(parallel = TRUE, cpus = ncor)
-    rail_rds <- st_read(file.path(anthro_dir, "rail_rds.gpkg")) 
-    
-    sfExport(list = c("ncor", "usa_shp", "rail_rds", "ras_mask"))
-    rail_rst <- sfLapply(1:ncor, shp_rst, y = rail_rds, lvl = "bool_rrds", j = ras_mask)
+if (!file.exists(file.path(transportation_dist_dir, "dis_railroads.tif"))) {
+
+    rail_rds_ed <- rail_rds %>%
+      st_transform(p4string_ed)
+
+    sfInit(parallel = TRUE, cpus = ncor)
+
+    sfExport(list = c("ncor", "usa_shp", "rail_rds_ed", "ras_mask"))
+    rail_rst <- sfLapply(1:ncor, shp_rst, y = rail_rds_ed, lvl = "bool_rrds", j = ras_mask)
     sfStop()
     dis_railroads <- combine_rst(rail_rst)
-    writeRaster(dis_railroads, filename = file.path(anthro_dir, paste0("dis_railroads", ".tif")),
+    writeRaster(dis_railroads, filename = file.path(transportation_dist_dir, paste0("dis_railroads", ".tif")),
                 format = "GTiff")
+} else {
+  dis_railroads <- raster(filename = file.path(transportation_dist_dir, paste0("dis_railroads", ".tif")))
 }
 
 
 # Calculate distance to primary roads
-if (file.exists(file.path(anthro_dir, "dis_primary_rds.tif"))) {
-  
-  dis_primary_rds <- raster(file.path(anthro_dir, "dis_primary_rds.tif")) 
-  
-  } else {
-    if (!exists("primary_rds")) {
-      primary_rds <- st_read(file.path(anthro_dir, "primary_rds.gpkg"))
-    }
-    
+if (!file.exists(file.path(transportation_dist_dir, "dis_primary_rds.tif"))) {
+
+    primary_rds_ed <- primary_rds %>%
+      st_transform(p4string_ed)
+
     sfInit(parallel = TRUE, cpus = ncor)
-    sfExport(list = c("ncor", "usa_shp", "primary_rds", "ras_mask"))
-    prds_rst <- sfLapply(1:ncor, shp_rst, y = primary_rds, lvl = "bool_prds", j = ras_mask)
+    sfExport(list = c("ncor", "usa_shp", "primary_rds_ed", "ras_mask"))
+    prds_rst <- sfLapply(1:ncor, shp_rst, y = primary_rds_ed, lvl = "bool_prds", j = ras_mask)
     sfStop()
     dis_primary_rds <- combine_rst(prds_rst)
-    writeRaster(dis_primary_rds, filename = file.path(anthro_dir, paste0("dis_primary_rds", ".tif")),
+    writeRaster(dis_primary_rds, filename = file.path(transportation_dist_dir, paste0("dis_primary_rds", ".tif")),
                 format = "GTiff", overwrite=TRUE)
-  }
+} else {
+  dis_primary_rds <- raster(filename = file.path(transportation_dist_dir, paste0("dis_primary_rds", ".tif")))
+}
 
 # Calculate distance to secondary roads
-if (file.exists(file.path(anthro_dir, "dis_secondary_rds.tif"))) {
-  
-  dis_secondary_rds <- raster(file.path(anthro_dir, "dis_secondary_rds.tif")) 
-  
-  } else {
-    if (! exists("secondary_rds")) {
-      secondary_rds <- st_read(file.path(anthro_dir, "secondary_rds.gpkg"))
-    }
-    
+if (!file.exists(file.path(transportation_dist_dir, "dis_secondary_rds.tif"))) {
+
+    secondary_rds_ed <- secondary_rds %>%
+      st_transform(p4string_ed)
+
     sfInit(parallel = TRUE, cpus = ncor)
-    sfExport(list = c("ncor", "usa_shp", "secondary_rds", "ras_mask"))
-    srds_rst <- sfLapply(1:ncor, shp_rst, y = secondary_rds, lvl = "bool_srds", j = ras_mask)
+    sfExport(list = c("ncor", "usa_shp", "secondary_rds_ed", "ras_mask"))
+    srds_rst <- sfLapply(1:ncor, shp_rst, y = secondary_rds_ed, lvl = "bool_srds", j = ras_mask)
     sfStop()
     dis_secondary_rds <- combine_rst(srds_rst)
-    writeRaster(dis_secondary_rds, filename = file.path(anthro_dir, paste0("dis_secondary_rds", ".tif")),
+    writeRaster(dis_secondary_rds, filename = file.path(transportation_dist_dir, paste0("dis_secondary_rds", ".tif")),
                 format = "GTiff", overwrite=TRUE)
-  }
+} else {
+  dis_secondary_rds <- raster(filename = file.path(transportation_dist_dir, paste0("dis_secondary_rds", ".tif")))
+}
 
-# Calculate distance to primary and secondary roads
-if (file.exists(file.path(anthro_dir, "dis_all_rds.tif"))) {
-  
-  dis_all_rds <- raster(file.path(anthro_dir, "dis_all_rds.tif")) 
-  
-  } else {
-    if (!exists("all_rds")) {
-      if (!exists("primary_rds")) {
-        primary_rds <- st_read(file.path(anthro_dir, "primary_rds.gpkg"))
-      }
-      if (! exists("secondary_rds")) {
-        secondary_rds <- st_read(file.path(anthro_dir, "secondary_rds.gpkg"))
-      }
-      
-      psrds <- primary_rds %>%
-        select(-starts_with("bool"))
-      ssrds <- secondary_rds  %>%
-        select(-starts_with("bool"))
-      
-      all_rds <- dplyr::bind_rows(psrds, ssrds) %>%
-        mutate(bool_ards = 1)
-    }
-    
-    sfInit(parallel = TRUE, cpus = ncor)
-    sfExport(list = c("ncor", "usa_shp", "all_rds", "ras_mask"))
-    ards_rst <- sfLapply(1:ncor, shp_rst, y = all_rds, lvl = "bool_ards", j = ras_mask)
-    sfStop()
-    dis_all_rds <- combine_rst(ards_rst)
-    writeRaster(dis_all_rds, filename = file.path(anthro_dir, paste0("dis_all_rds", ".tif")),
-                format = "GTiff", overwrite=TRUE)
-  }
+# Calculate distance to tertiary roads
+if (!file.exists(file.path(transportation_dist_dir, "dis_tertiary_rds.tif"))) {
 
+  tertiary_rds_ed <- tertiary_rds %>%
+    st_transform(p4string_ed)
 
-
+  sfInit(parallel = TRUE, cpus = ncor)
+  sfExport(list = c("ncor", "usa_shp", "tertiary_rds_ed", "ras_mask"))
+  srds_rst <- sfLapply(1:ncor, shp_rst, y = tertiary_rds_ed, lvl = "bool_srds", j = ras_mask)
+  sfStop()
+  dis_tertiary_rds <- combine_rst(srds_rst)
+  writeRaster(dis_tertiary_rds, filename = file.path(transportation_dist_dir, paste0("dis_tertiary_rds", ".tif")),
+              format = "GTiff", overwrite=TRUE)
+} else {
+  dis_tertiary_rds <- raster(filename = file.path(transportation_dist_dir, paste0("dis_tertiary_rds", ".tif")))
+}
