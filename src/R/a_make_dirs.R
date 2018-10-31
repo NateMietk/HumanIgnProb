82a539d2dae21622606f48a5da701b627027e85f
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-10-31 19:55:09 +0000

    cleaned up dirs

--- src/R/a_make_dirs.R
+++ src/R/a_make_dirs.R
@@ -7,8 +7,7 @@ lapply(x, library, character.only = TRUE, verbose = FALSE)
 # load all functions
 source('src/functions/helper_functions.R')
 
-# key rojections
-p4string_ed <- "+proj=eqdc +lat_0=0 +lon_0=0 +lat_1=33 +lat_2=45 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"   #http://spatialreference.org/ref/esri/102005/
+# key projections
 p4string_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"   #http://spatialreference.org/ref/sr-org/6903/
 
 # define the amount of cores st_par runs on
@@ -16,7 +15,6 @@ ncor <- parallel::detectCores()
 
 # create main directories
 prefix <- ("data")
-ancillary_dir <- file.path(prefix, "ancillary")
 summary_dir <- file.path(prefix, "extractions")
 processed_dir <- file.path(prefix, 'processed')
 
@@ -28,13 +26,7 @@ ecoregionl4_prefix <- file.path(raw_prefix, "us_eco_l4")
 fpa_prefix <- file.path(raw_prefix, "fpa-fod")
 roads_prefix <- file.path(raw_prefix, "tlgdb_2015_a_us_roads")
 rails_prefix <- file.path(raw_prefix, "tlgdb_2015_a_us_rails")
-nlcd_prefix <- file.path(raw_prefix, "nlcd_2011_landcover_2011_edition_2014_10_10")
-nlcd92_prefix <- file.path(raw_prefix, "nlcd_1992")
-nlcd01_prefix <- file.path(raw_prefix, "nlcd_2001_landcover_2011_edition_2014_10_10")
-nlcd06_prefix <- file.path(raw_prefix, "nlcd_2006_landcover_2011_edition_2014_10_10")
-nlcd_pdi_01_prefix <- file.path(raw_prefix, "nlcd_2001_impervious_2011_edition_2014_10_10")
-nlcd_pdi_06_prefix <- file.path(raw_prefix, "nlcd_2006_impervious_2011_edition_2014_10_10")
-nlcd_pdi_11_prefix <- file.path(raw_prefix, "nlcd_2011_impervious_2011_edition_2014_10_10")
+landcover_dir <- file.path(raw_prefix, 'landcover')
 
 pd_prefix <- file.path(raw_prefix, "county_pop")
 iclus_prefix <- file.path(raw_prefix, 'housing_den')
@@ -50,7 +42,7 @@ transportation_dir <- file.path(processed_dir, 'transportation')
 transportation_density_dir <- file.path(transportation_dir, 'density')
 transportation_processed_dir <- file.path(transportation_dir, 'processed')
 transportation_dist_dir <- file.path(transportation_dir, 'distance')
-
+proc_gridded_census <- file.path(anthro_proc_dir, 'gridded_census')
 per_state <- file.path(transportation_density_dir, "per_state")
 
 # create direcotires to hold climate summary outputs
@@ -67,9 +59,8 @@ ztrax_dir <- file.path(anthro_dir, "ztrax")
 raw_ztrax_dir <- file.path(ztrax_dir, "raw_built_up_gpkg")
 stacked_ztrax_rst_dir <- file.path(ztrax_dir, "stacked_ztrax_rst")
 
-fishnet_path <- file.path(ancillary_dir, "fishnet")
-
 # for pushing and pulling to s3 using the system function
+s3_base <- 's3://earthlab-modeling-human-ignitions/'
 s3_anc_prefix <- 's3://earthlab-modeling-human-ignitions/ancillary/'
 s3_proc_prefix <- 's3://earthlab-modeling-human-ignitions/processed/'
 s3_raw_prefix <- 's3://earthlab-modeling-human-ignitions/raw/'
@@ -80,14 +71,12 @@ s3_proc_anthro <- 's3://earthlab-modeling-human-ignitions/anthro/'
 
 # Check if directory exists for all variable aggregate outputs, if not then create
 var_dir <- list(prefix, raw_prefix, us_prefix, ecoregion_prefix, roads_prefix, summary_dir,
-                fpa_prefix, rails_prefix, pd_prefix, iclus_prefix, climate_prefix,
-                nlcd_prefix,nlcd92_prefix ,nlcd01_prefix ,nlcd06_prefix , elev_prefix,
-                tl_prefix, ancillary_dir, bounds_dir, anthro_dir, fishnet_path, processed_dir, summaries_dir,
-                nlcd_pdi_01_prefix, nlcd_pdi_06_prefix, nlcd_pdi_11_prefix, summary_mean, transportation_dist_dir,
+                fpa_prefix, rails_prefix, pd_prefix, iclus_prefix, climate_prefix, elev_prefix,
+                tl_prefix, bounds_dir, anthro_dir, processed_dir, summaries_dir, summary_mean, transportation_dist_dir,
                 summary_95th, summary_numdays95th, terrain_dir, transportation_dir, anthro_proc_dir,
                 transportation_density_dir, transportation_processed_dir, anthro_dir, anthro_state_extract,
                 terrain_extract, anthro_extract, per_state,ecoregionl4_prefix, ztrax_dir, raw_ztrax_dir,
-                stacked_ztrax_rst_dir)
+                stacked_ztrax_rst_dir, proc_gridded_census, landcover_dir)
 
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
 
