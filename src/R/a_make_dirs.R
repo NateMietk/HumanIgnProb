239587abfe1c8f32e50bbdbb17f9819aea2e17b8
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-01-10 20:34:11 +0000

    added ztrax and wui dirs

--- src/R/a_make_dirs.R
+++ src/R/a_make_dirs.R
@@ -29,7 +29,6 @@ fpa_prefix <- file.path(raw_prefix, "fpa-fod")
 roads_prefix <- file.path(raw_prefix, "tlgdb_2015_a_us_roads")
 rails_prefix <- file.path(raw_prefix, "tlgdb_2015_a_us_rails")
 landcover_dir <- file.path(raw_prefix, 'landcover')
-
 pd_prefix <- file.path(raw_prefix, "county_pop")
 iclus_prefix <- file.path(raw_prefix, 'housing_den')
 elev_prefix <- file.path(raw_prefix, 'metdata_elevationdata')
@@ -40,6 +39,7 @@ bounds_dir <- file.path(prefix, 'bounds')
 # create processed directories
 terrain_dir <- file.path(processed_dir, 'terrain')
 anthro_proc_dir <- file.path(processed_dir, 'anthro')
+wui_proc_dir <- file.path(anthro_proc_dir, 'wui')
 transportation_dir <- file.path(processed_dir, 'transportation')
 transportation_density_dir <- file.path(transportation_dir, 'density')
 transportation_processed_dir <- file.path(transportation_dir, 'processed')
@@ -64,10 +64,14 @@ anthro_dir <- file.path(prefix, "anthro")
 ztrax_dir <- file.path(anthro_dir, "ztrax")
 raw_ztrax_dir <- file.path(ztrax_dir, "raw_built_up_gpkg")
 stacked_ztrax_rst_dir <- file.path(ztrax_dir, "stacked_ztrax_rst")
+count_ztrax_dir <- file.path(ztrax_dir, 'ztrax_raw_count_1980_2015_1k')
+cumsum_ztrax_dir <- file.path(ztrax_dir, 'ztrax_raw_cumsum_1980_2015_1k')
 
 # Monthly stacks
 terrain_monthly_dir <- file.path(terrain_dir, 'monthly')
 bounds_monthly_dir <- file.path(proc_bounds_dir, 'monthly')
+anthro_monthly_proc_dir <- file.path(anthro_proc_dir, 'monthly')
+transport_monthly_proc_dir <- file.path(transportation_processed_dir, 'monthly')
 
 # for pushing and pulling to s3 using the system function
 s3_base <- 's3://earthlab-modeling-human-ignitions/'
@@ -80,13 +84,13 @@ s3_proc_bounds <- 's3://earthlab-modeling-human-ignitions/bounds/'
 s3_proc_anthro <- 's3://earthlab-modeling-human-ignitions/anthro/'
 
 # Check if directory exists for all variable aggregate outputs, if not then create
-var_dir <- list(prefix, raw_prefix, us_prefix, ecoregion_prefix, roads_prefix, summary_dir,
+var_dir <- list(prefix, raw_prefix, us_prefix, ecoregion_prefix, roads_prefix, summary_dir, count_ztrax_dir,
                 fpa_prefix, rails_prefix, pd_prefix, iclus_prefix, climate_prefix, elev_prefix,
                 tl_prefix, bounds_dir, anthro_dir, processed_dir, summaries_dir, summary_mean, transportation_dist_dir,
                 summary_95th, summary_numdays95th, terrain_dir, transportation_dir, anthro_proc_dir,
                 transportation_density_dir, transportation_processed_dir, anthro_dir, anthro_state_extract,
                 terrain_extract, anthro_extract, per_state,ecoregionl4_prefix, ztrax_dir, raw_ztrax_dir,
-                stacked_ztrax_rst_dir, proc_gridded_census, landcover_dir, proc_landcover, fire_dir)
+                stacked_ztrax_rst_dir, proc_gridded_census, landcover_dir, proc_landcover, fire_dir, 
+                anthro_monthly_proc_dir, transport_monthly_proc_dir, wui_proc_dir, wui_proc_dir, cumsum_ztrax_dir)
 
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
-
