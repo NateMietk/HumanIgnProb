f9366c343ff971e7934d6c6568a5d91d1e44cdbe
joemcglinchy <joseph.mcglinchy@colorado.edu>
2019-03-11 11:23:00 -0600

    scripts to rename s3 files

--- src/python/rename_s3.py
+++ src/python/rename_s3.py
@@ -1,50 +1,50 @@
 import os, sys
 import subprocess
+import itertools
+
 
 ## extract filename and re-order, e.g., rename all Equipment_Use to EquipmentUse, Debris_Burning to DebrisBurning
-x_var_dir = '../../../data/x_tiles'
+s3_folder = 's3://earthlab-modeling-human-ignitions/tiles/x_var'
 
-aet = glob(x_var_dir + "/{}*.tif".format('aet'))
-landmask = glob(x_var_dir + "/{}*.tif".format('land-mask'))
+rename_vars = ['aet-95th', 'aet-mean',
+               'def-95th', 'def-mean',
+               'ffwi-95th', 'ffwi-mean',
+               'fm100-95th', 'fm100-mean',
+               'pdsi-95th', 'pdsi-mean',
+               'pr-95th', 'pr-mean',
+               'vpd-95th', 'vpd-mean',
+               'tmmx-95th', 'tmmx-mean',
+               'vs-95th', 'vs-mean']
 
-f=aet[0]
-bname = os.path.basename(f)
-dname = os.path.dirname(f)
+# set up some file path needs
+tiles = list(range(1,151))
+months = list(range(1,13))
+years = list(range(1992,2016))
+year_month_tile = list(itertools.product(years, months, tiles))
+for var in rename_vars:
+    for year,month,tile in year_month_tile:
+        
+        # construct 'old' filename
+        var_split = var.split('-')
+        fi_old = f'{var_split[0]}_{year}_{var_split[1]}_{month}_t{tile}.tif'
 
-fname_parts = bname.split('_')
-print(fname_parts)
-index_correct = [0,2,1,3,4]
-fname_parts_correct = [fname_parts[i] for i in index_correct]
-print(fname_parts_correct)
-# the first two items should be concatenated
-print(fname_parts_correct[2:])
-first_str = fname_parts_correct[0] + '-' + fname_parts_correct[1]
-fname_parts_correct = [first_str] + fname_parts_correct[2:]
-print(fname_parts_correct)
-new_fname = '_'.join(fname_parts_correct)
-print(new_fname)
-
-# rename
-for f in aet:
-    bname = os.path.basename(f)
-    dname = os.path.dirname(f)
-    
-    fname_parts = bname.split('_')
-    index_correct = [0,2,1,3,4]
-    fname_parts_correct = [fname_parts[i] for i in index_correct]
-    
-    # the first two items should be concatenated
-    first_str = fname_parts_correct[0] + '-' + fname_parts_correct[1]
-    fname_parts_correct = [first_str] + fname_parts_correct[2:]
-    new_fname = '_'.join(fname_parts_correct)
-    
-    os.rename(f, os.path.join(dname,new_fname))
-    
-## set up some parameters
-s3_folder = 's3://earthlab-modeling-human-ignitions/<folder>'
-old_fi = s3_folder + '/{}'.format(old_file)
-new_fi = s3_folder + '/{}'.format(new_file)
-
-## produce cmd string and use subprocess to call it
-#sub_str = 'aws s3 --recursive mv {old_fi} {new_fi}'
-#subprocess.call(sub_str)
+        # rearrange filename parts
+        fname_parts = fi_old.split('_')
+        index_correct = [0,2,1,3,4]
+        fname_parts_correct = [fname_parts[i] for i in index_correct]
+        first_str = fname_parts_correct[0] + '-' + fname_parts_correct[1]
+        fname_parts_correct = [first_str] + fname_parts_correct[2:]
+        new_fname = '_'.join(fname_parts_correct)
+        
+        #print(fi_old, ">>>", new_fname)
+        
+        # construct S3 URIs
+        old_fi = s3_folder + '/{}'.format(fi_old)
+        new_fi = s3_folder + '/{}'.format(new_fname)
+              
+        # call aws mv to rename file
+        sub_str = f'aws s3 --recursive mv {old_fi} {new_fi}'
+        print(f'{old_fi} >>> {new_fi}')
+        subprocess.call(sub_str)
+        print('done\n')
+        
---
+++ src/python/rename_s3_multi.py
@@ -0,0 +1,60 @@
+import os, sys
+import subprocess
+import itertools
+import multiprocessing as mp
+from functools import partial
+
+def proc_fun(var):
+    
+    s3_folder = 's3://earthlab-modeling-human-ignitions/tiles/x_var'
+
+    tiles = list(range(1,151))
+    months = list(range(1,13))
+    years = list(range(1992,2016))
+    year_month_tile = list(itertools.product(years, months, tiles))
+    for year,month,tile in year_month_tile:
+
+        # construct 'old' filename
+        var_split = var.split('-')
+        fi_old = f'{var_split[0]}_{year}_{var_split[1]}_{month}_t{tile}.tif'
+
+        # rearrange filename parts
+        fname_parts = fi_old.split('_')
+        index_correct = [0,2,1,3,4]
+        fname_parts_correct = [fname_parts[i] for i in index_correct]
+        first_str = fname_parts_correct[0] + '-' + fname_parts_correct[1]
+        fname_parts_correct = [first_str] + fname_parts_correct[2:]
+        new_fname = '_'.join(fname_parts_correct)
+
+        #print(fi_old, ">>>", new_fname)
+
+        # construct S3 URIs
+        old_fi = s3_folder + '/{}'.format(fi_old)
+        new_fi = s3_folder + '/{}'.format(new_fname)
+
+        # call aws mv to rename file
+        sub_str = f'aws s3 --recursive mv {old_fi} {new_fi}'
+        subprocess.call(sub_str)
+        
+        
+
+
+if __name__ == '__main__':
+    rename_vars = ['aet-95th', 'aet-mean',
+                   'def-95th', 'def-mean',
+                   'ffwi-95th', 'ffwi-mean',
+                   'fm100-95th', 'fm100-mean',
+                   'pdsi-95th', 'pdsi-mean',
+                   'pr-95th', 'pr-mean',
+                   'vpd-95th', 'vpd-mean',
+                   'tmmx-95th', 'tmmx-mean',
+                   'vs-95th', 'vs-mean']
+
+    nproc = max(mp.cpu_count()-2, 2)
+    pool = mp.Pool(nproc)
+    
+    vals = pool.map(partial(proc_fun), rename_vars[1:])
+    
+    # close the pool
+    pool.close()
+    pool.join()
